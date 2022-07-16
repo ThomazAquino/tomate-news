39b4206a4076e4e6cf43ca69a9e1764cc1a0a01b
ThomazAquino <thomazmanoel.aquino@gmail.com>
2022-07-15 21:39:37 -0300

    chore: add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
+import Head from 'next/head'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Inico | Ig.news</title>
+    </Head>
     <h1>tomate</h1>
+    </>
   )
 }
