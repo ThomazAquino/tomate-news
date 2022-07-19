34e52359c6266b3513818f1de63ce54dbc1ed79c
ThomazAquino <thomazmanoel.aquino@gmail.com>
2022-07-19 07:57:42 -0300

    feat(stripe): start payment method

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,43 @@
-import styles from "./styles.module.scss";
+import { signIn, useSession } from 'next-auth/react';
 
-interface ISubscribeButtonProps {
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
+import styles from './styles.module.scss';
+
+interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton({ priceId }: ISubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe?.redirectToCheckout({ sessionId });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button 
+      type="button" 
+      className={styles.subscribeButton} 
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
-  );
+  )
 }
